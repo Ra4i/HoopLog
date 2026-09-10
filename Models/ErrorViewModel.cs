8d4297fc5000def4da2dff3d5a492019642b8837
Ra4i <dttodorov2009@gmail.com>
2026-09-10 20:57:56 +0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HoopLog.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
